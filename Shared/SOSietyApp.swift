0db0d1013f3e2a88b65cd091816fa25338b7592c
likhv <likh_v@yahoo.com>
2022-02-07 17:14:45 +0100

    Initial Commit

---
+++ Shared/SOSietyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOSietyApp.swift
+//  Shared
+//
+//  Created by Vladislav Likh on 07/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SOSietyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
